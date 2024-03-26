0c17c33605471599983f05c55eb7538704876a89
Pawan Kumar Gupta <56015995+dcod3r@users.noreply.github.com>
2024-03-26 12:15:04 +0530

    Time: 16 ms (15.66%) | Memory: 10.3 MB (12.05%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+    std::string strX = std::to_string(x);
+        return strX == std::string(strX.rbegin(), strX.rend());
+    }
+};
