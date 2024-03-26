7c7c0a374a776a2c2cda401834404192ada05e3d
Pawan Kumar Gupta <56015995+dcod3r@users.noreply.github.com>
2024-03-26 17:33:15 +0530

    Time: 12 ms (33.79%) | Memory: 8.2 MB (79.41%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,7 +2,30 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-    std::string strX = std::to_string(x);
-        return strX == std::string(strX.rbegin(), strX.rend());
+        if(x<0)
+            return false;
+        if(x == 0)
+            return true;
+        int temp = x, n=0;
+        while(temp){
+            temp = temp / 10;
+            n++;
+        }
+        int a[n];
+        int i=0;
+        temp = x;
+        while(temp){
+            a[i++] = temp % 10;
+            temp = temp / 10;
+        }
+        i--;
+        int j = 0;
+        while(j<=i)
+        {
+            if(a[j++] != a[i--])
+                return false;
+        }
+
+        return true;
     }
 };
