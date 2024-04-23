22a51039d745ab9ddb0229fa2ad802cf59f5147e
Pawan Kumar Gupta <56015995+dcod3r@users.noreply.github.com>
2024-04-23 09:27:28 +0530

    Time: 9 ms (51.14%) | Memory: 9.7 MB (18.85%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,11 +2,27 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         // O(nlogn)
-         sort(s.begin(), s.end());
-         sort(t.begin(), t.end());
-         if(s == t)
-             return true;
-         else 
-             return false;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s == t)
+        //     return true;
+        // else 
+        //     return false;
+        
+        // O(n)
+        
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> mp1, mp2;
+		// Count number of character occurs in both the strings.
+        for (char c: s)
+            mp1[c]++;
+        for (char c: t)
+            mp2[c]++;
+	
+        for (int i = 0; i < s.size(); i++)
+            if (mp1[s[i]] != mp2[s[i]])
+                return false;
+        return true;
     }
 };
